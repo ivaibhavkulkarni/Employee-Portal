54961683a0cf73963b76fd85d4c2c35ede521add
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2024-11-05 10:47:08 +0530

    DB bug

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use('/api/userRoutes', userRoutes);
 app.use('/api/employeeRoutes', employeeRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
